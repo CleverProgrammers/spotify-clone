947e5a8f7461f1242d14b2550e3827999f355c97
Keshaw Thakur <keshavthakur676@gmail.com>
2020-10-01 10:19:23 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ function Login() {
   return (
     <div className="login">
       <img
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        src="https://cdn.dribbble.com/users/441326/screenshots/3165191/spotify-gif---oliver-keane.gif"
         alt=""
       />
       <a href={accessUrl}>LOGIN TO SPOTIFY</a>
